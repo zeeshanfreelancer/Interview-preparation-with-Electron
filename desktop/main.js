e921158eed9f1d1d61ce2beae4409bbcda11754c
zeeshanfreelancer <zeeshankhan469224@gmail.com>
2026-01-26 18:00:21 +0500

    default menu hidden on desktop

--- desktop/main.js
+++ desktop/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
 
 function createWindow() {
@@ -9,6 +9,9 @@ function createWindow() {
       preload: path.join(__dirname, "preload.js")
     }
   });
+
+  // Remove menu bar
+  Menu.setApplicationMenu(null);
 
   // DEV mode
   win.loadURL("http://localhost:5173");
